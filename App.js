3c57c8d52538641515e1001b9e3c5287009cef79
Boluwatife <Boluwatifeoladejo@gmail.com>
2023-11-21 13:12:25 +0100

    app 2nd class

--- App.js
+++ App.js
@@ -1,20 +1,61 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react'
+import { 
+  View, 
+  Text, 
+  Button, 
+  Image,
+  TouchableOpacity 
+} from 'react-native'
+import nitda from './assets/nitda.png'
 
-export default function App() {
+const App = () => {
+
+  const [isShow, setIsShow] = useState('')
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{
+        flex: 1,
+        backgroundColor: 'green',
+        alignItems: 'center',
+        justifyContent: 'center'
+      }}
+    >
+      <TouchableOpacity
+        onPress={() => setIsShow('I just pressed an image')}
+      >
+        <Image 
+          source={nitda}
+          style={{
+            height: 100
+          }}
+        />
+      </TouchableOpacity>
+
+      <Text style={{
+        color: 'white',
+        fontSize: 40
+        }}
+      >
+        Hello, Nitda
+      </Text>
+      <Button 
+        title='Change Name'
+        onPress={() => setIsShow('I just pressed a button')}
+      />
+
+      {isShow && (
+        <Text
+          style={{
+            color: 'white',
+            fontSize: 30,
+            marginTop: 12
+          }}
+        >
+          {isShow}
+        </Text>
+      )}
     </View>
-  );
+  )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App
